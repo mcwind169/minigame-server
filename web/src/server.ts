50f7a1366c30cd90ecc22554fd1ea6d6290b9c3a
mcwind169 <mcwind@gmail.com>
2024-08-06 19:02:06 +0800

    redis inclued ok!

--- web/src/server.ts
+++ web/src/server.ts
@@ -1,13 +1,34 @@
-// src/index.js
-import express, { Express, Request, Response } from "express";
 
-const app: Express = express();
-const port = 3000;
+import express from 'express';
+import Redis from 'ioredis';
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("mini Game Server123");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Create a Redis client
+const redis = new Redis(6379,'redis');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello Redis with Express.js and TypeScript!');
+});
+
+// Example of caching data
+app.get('/cache', async (req, res) => {
+  const cachedData = await redis.get('cachedData');
+
+  if (cachedData) {
+    // If data exists in the cache, return it
+    res.send(JSON.parse(cachedData));
+  } else {
+    // If data is not in the cache, fetch it from the source
+    const dataToCache = { message: 'Data to be cached' };
+    await redis.set('cachedData', JSON.stringify(dataToCache), 'EX', 6); // Cache for 1 hour
+    res.send('no cache found, create new...');
+  }
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
